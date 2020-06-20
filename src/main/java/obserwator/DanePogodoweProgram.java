6843b4cc1672f6b55c64665bdf54fe9c24e4f3da
Marcin Szupke <marcin.szupke@gmail.com>
2020-06-20 14:49:20 +0200

    zadanie - stacja pogodowa

---
+++ src/main/java/obserwator/DanePogodoweProgram.java
@@ -0,0 +1,19 @@
+package obserwator;
+
+public class DanePogodoweProgram {
+
+//    public static void main(String[] args) {
+//        DanePogodowe danePogodowe = new DanePogodowe();
+//        WarunkiBiezace warunkiBiezace = new WarunkiBiezace(danePogodowe);
+//        Statystyka statystyka = new Statystyka(danePogodowe);
+//        WarunkiBiezaceCisnienia warunkiBiezaceCisnienia = new WarunkiBiezaceCisnienia(danePogodowe);
+//
+//        danePogodowe.ustawStan(80, 65, 30.4f);
+//        danePogodowe.ustawStan(82, 70, 29.2f);
+//        danePogodowe.ustawStan(78, 90, 30.4f);
+//
+//        WarunkiBiezaceCisnienia warunkiBiezaceCisnienia1 = new WarunkiBiezaceCisnienia(danePogodowe);
+//
+//        danePogodowe.ustawStan(78, 90, 30.4f);
+//    }
+}
